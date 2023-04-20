1b194a55572e3770d10087d4cc4ac396156c8e54
alto <alto.martin@outlook.com>
2023-04-20 14:37:37 +0800

    feat: about page for app info

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'pages/about'
+  get 'about', :to => 'pages#about'
+  root 'pages#about'
 end
